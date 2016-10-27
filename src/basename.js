6880d229610800465a679c9ca8a499a0098695f7
Ryan Roemer <ryan@loose-bits.com>
2016-10-27 15:16:32 -0700

    Up and running with victory site. Ready for charts.

---
+++ src/basename.js
@@ -0,0 +1,3 @@
+// TODO: UPDATE_FOR_CHARTS
+const basename = process.env.NODE_ENV === "production" ? "/open-source/victory" : "";
+export default basename;
---
+++ static/README.md
@@ -0,0 +1,5 @@
+// TODO: UPDATE_FOR_CHARTS
+
+Place all static assets here.
+
+Then remove this file.
