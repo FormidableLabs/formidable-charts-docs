a95aea7bf6d40aaac4349ae97f8bb20d82c92803
Paula Lavalle <lavalle.paula@gmail.com>
2016-11-09 10:07:36 -0800

    Add Standalone docs

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,6 @@
 import Index from "formidable-charts/docs/index.md";
 import API from "formidable-charts/docs/api.md";
+import Standalone from "formidable-charts/docs/standalone.md";
 import Themes from "formidable-charts/docs/themes.md";
 
 export const config = [
@@ -8,14 +9,17 @@ export const config = [
     slug: "index",
     category: "introduction",
     docs: Index
-  },
-  {
+  }, {
     text: "API",
     slug: "api",
     category: "base",
     docs: API
-  },
-  {
+  }, {
+    text: "Standalone",
+    slug: "standalone",
+    category: "base",
+    docs: Standalone
+  }, {
     text: "Themes",
     slug: "themes",
     category: "base",
